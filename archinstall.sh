21918d0dee376a596f5737a1289e3bff996c5623
sxrrycorpse <15416844-sxrrycorpse@users.noreply.gitlab.com>
2025-09-24 21:46:15 +0530

    add format & mount filesystems

--- archinstall.sh
+++ archinstall.sh
@@ -5,8 +5,16 @@
 # Update mirrorlist
 reflector --country India --latest 10 --sort rate --save /etc/pacman.d/mirrorlist --verbose
 
-# Disk Partition
+# Disk partition
 sgdisk -Z /dev/vda
 sgdisk -n 1::+1G -c 1:"EFI BOOT Partition" -t 1:ef00 /dev/vda
 sgdisk -n 2:: -c 2:"Linux Partition" -t 2:8300 /dev/vda
 sgdisk -p /dev/vda
+
+# Format partitions
+mkfs.ext4 /dev/vda2
+mkfs.fat -F 32 /dev/vda1
+
+# Mount filesystems
+mount /dev/vda2 /mnt
+mount --mkdir /dev/vda1 /mnt/boot
