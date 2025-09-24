0af4405b480c3b195904b63392101ddbcc241183
sxrrycorpse <15416844-sxrrycorpse@users.noreply.gitlab.com>
2025-09-24 21:34:47 +0530

    add disk partition

--- archinstall.sh
+++ archinstall.sh
@@ -4,3 +4,9 @@
 
 # Update mirrorlist
 reflector --country India --latest 10 --sort rate --save /etc/pacman.d/mirrorlist --verbose
+
+# Disk Partition
+sgdisk -Z /dev/vda
+sgdisk -n 1::+1G -c 1:"EFI BOOT Partition" -t 1:ef00 /dev/vda
+sgdisk -n 2:: -c 2:"Linux Partition" -t 2:8300 /dev/vda
+sgdisk -p /dev/vda
