23e6c7724e815514e4e7060f0dad709699414610
sxrrycorpse <15416844-sxrrycorpse@users.noreply.gitlab.com>
2025-09-25 18:03:33 +0530

    add localization && hostname

--- archinstall.sh
+++ archinstall.sh
@@ -31,3 +31,11 @@ arch-chroot /mnt
 # Timezone
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
+
+# Localization
+sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+# Hostname
+echo "aetherius" > /etc/hostname
