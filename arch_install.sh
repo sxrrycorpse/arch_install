ea387b4e24ca5caffd73f60684ba6bbbc95fcd2e
sxrrycorpse <sxrrycorpse|114552693+sxrrycorpse@users.noreply.github.com>
2025-10-19 19:09:35 +0530

    update pacstrap packages

--- arch_install.sh
+++ arch_install.sh
@@ -20,7 +20,7 @@ mount /dev/vda2 /mnt
 mount --mkdir /dev/vda1 /mnt/boot
 
 # Install essential packages
-pacstrap -K /mnt base base-devel linux linux-firmware amd-ucode neovim grub man-db tealdeer
+pacstrap -K /mnt base base-devel linux linux-firmware amd-ucode neovim man-db tealdeer grub
 
 # System configuration
 genfstab -U /mnt >> /mnt/etc/fstab
