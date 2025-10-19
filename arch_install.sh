09ecf431e563c8e80f8064c9c6b1659d1fb9990c
sxrrycorpse <sxrrycorpse|114552693+sxrrycorpse@users.noreply.github.com>
2025-10-19 17:37:19 +0530

    fix grub-mkconfig

--- arch_install.sh
+++ arch_install.sh
@@ -41,4 +41,4 @@ echo "aetherius" > /mnt/etc/hostname
 
 # Grub-install
 arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
